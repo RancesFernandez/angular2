c8cc8a702f69c55dccbc51afcd4847eb5882c8d2
John Doe <carlosrancesfb1991@gmail.com>
2021-09-27 13:08:57 -0300

    latest commit

---
+++ formularios/src/app/services/pais.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  constructor( private http: HttpClient) { }
+
+  getPaises(){
+    return this.http.get('https://www.zipcodeapi.com/');
+  }
+}
