c210b1780a551c03c9f6a17ae2b293e7de81e0a7
John Doe <carlosrancesfb1991@gmail.com>
2021-11-08 15:03:58 -0300

    lastest commit

--- formularios/src/app/models/heroe.model.ts
+++ formularios/src/app/models/heroe.model.ts
@@ -1,3 +1,7 @@
+
+
+
+
 export class HeroeModel {
 
   id?: string;
--- formularios/src/app/services/heroes.service.ts
+++ formularios/src/app/services/heroes.service.ts
@@ -36,6 +36,10 @@ export class HeroesService {
     return this.http.put(`${this.url }/Heroes/${heroe.id}.json`, heroeTemp);
   }
 
+  getHeroe(id: string){
+    return this.http.get(`${this.url}/heroes/${id}.json`);
+  }
+
   getHeroes(){
     return this.http.get(`${this.url}/Heroes.json`)
     .pipe(
